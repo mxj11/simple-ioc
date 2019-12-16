c7988cd6772047f843317e85dd009f74ed953b00
刘向昭 <739554776@qq.com>
2019-12-16 23:23:26 +0800

    完善ioc注解版本

--- simple-ioc-annotation/src/main/java/com/simple/ioc/SimpleAnnotationApplicationContext.java
+++ simple-ioc-annotation/src/main/java/com/simple/ioc/SimpleAnnotationApplicationContext.java
@@ -17,30 +17,26 @@ public class SimpleAnnotationApplicationContext {
     }
 
     private void initDependence(Object object) {
-        if (!CollectionUtils.isEmpty(contains)) {
-            contains.forEach((key,value) ->{
-                Field[] declaredFields = value.getClass().getDeclaredFields();
-                if (declaredFields != null) {
-                    for (Field field : declaredFields) {
-                        if (field == null) {
-                            continue;
-                        }
-                        Resource annotation = field.getAnnotation(Resource.class);
-                        if (annotation != null) {
-                            String name = field.getName();
-                            Object fieldObject = contains.get(name);
-                            if (fieldObject != null) {
-                                field.setAccessible(true);
-                                try {
-                                    field.set(object,fieldObject);
-                                } catch (IllegalAccessException e) {
-                                    e.printStackTrace();
-                                }
-                            }
+        Field[] declaredFields = object.getClass().getDeclaredFields();
+        if (declaredFields != null) {
+            for (Field field : declaredFields) {
+                if (field == null) {
+                    continue;
+                }
+                Resource annotation = field.getAnnotation(Resource.class);
+                if (annotation != null) {
+                    String name = field.getName();
+                    Object fieldObject = contains.get(name);
+                    if (fieldObject != null) {
+                        field.setAccessible(true);
+                        try {
+                            field.set(object, fieldObject);
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
                         }
                     }
                 }
-            });
+            }
         }
     }
 
@@ -66,7 +62,9 @@ public class SimpleAnnotationApplicationContext {
                 }
                 // 放入IOC容器
                 try {
-                    contains.put(beanId, classInfo.newInstance());
+                    Object object = classInfo.newInstance();
+                    contains.put(beanId, object);
+                    initDependence(object);
                 } catch (InstantiationException | IllegalAccessException e) {
                     e.printStackTrace();
                 }
@@ -87,7 +85,6 @@ public class SimpleAnnotationApplicationContext {
             throw new RuntimeException("beanId must not be empty");
         }
         if (contains.containsKey(beanId)) {
-            initDependence(contains.get(beanId));
             return contains.get(beanId);
         }
         throw new RuntimeException("get bean failed");
